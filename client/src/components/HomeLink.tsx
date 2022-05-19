a2b6d1fb37f31bd7b7a53dba4a8d85f63b5eaf14
newclarityex <69740123+newclarityex@users.noreply.github.com>
2022-05-19 03:07:37 -0400

    initial design for home

--- client/src/components/HomeLink.tsx
+++ client/src/components/HomeLink.tsx
@@ -4,10 +4,15 @@ import styles from './styles/HomeLink.module.css';
 export default function HomeLink(props: { url: string, title: string, description: string, icon: string }) {
     return (
         <>
-            <a href={props.url} class={styles.wrapper}>
-                <img src={props.icon} class={styles.bgIcon} alt={props.title} />
-                <img src={hexagon} class={styles.hexagon} />
-            </a>
+            <div class={styles.wrapper}>
+                <a href={props.url} class={styles.iconWrapper}>
+                    <img src={props.icon} class={styles.bgIcon} alt={props.title} />
+                    <img src={hexagon} class={styles.hexagon} />
+                </a>
+                <h2 class={styles.title}>{props.title}</h2>
+                <div class={styles.separator}></div>
+                <p class={styles.description}>{props.description}</p>
+            </div>
         </>
     );
 }
--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,12 +1,23 @@
 import HomeLink from "../components/HomeLink";
 import sortingIcon from "../assets/sorting.svg";
+import styles from "./styles/Home.module.css";
 
 export default function Home() {
     return (
         <>
-            <div>
-                <div>Human Computation Benchmark</div>
-                <HomeLink url="./sorting" icon={sortingIcon} title="Sorting" description="Can you sort a collection faster than your computer? Probably not, but go ahead and try anyways." />
+            <div class={styles.wrapper}>
+                <h1 class={styles.title}>Human Computation Benchmark</h1>
+                <div class={styles.optionsWrapper}>
+                    <div class={styles.optionsRow}>
+                        <HomeLink url="./sorting" icon={sortingIcon} title="Sorting" description="Sort the objects in ascending height by drag and drop" />
+                        <HomeLink url="./pathfinding" icon={sortingIcon} title="Pathfinding" description="Find your way around a maze using the arrow keys" />
+                    </div>
+                    <div class={styles.optionsRow}>
+                        <HomeLink url="./calculations" icon={sortingIcon} title="Calculations" description="Solve the math equations presented" />
+                        <HomeLink url="./sorting" icon={sortingIcon} title="Image Recognition" description="Select the option that matches the image shown" />
+                        <HomeLink url="./sorting" icon={sortingIcon} title="Object Search" description="Click the matching object in the sorted collection" />
+                    </div>
+                </div>
             </div>
         </>
     );
