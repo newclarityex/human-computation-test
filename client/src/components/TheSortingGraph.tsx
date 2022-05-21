22ce17133850bcbe6993b7148604db2df448c293
newclarityex <69740123+newclarityex@users.noreply.github.com>
2022-05-20 23:28:11 -0400

    Added sorts, needs refactoring + selection sort pretty broken

--- client/src/components/TheSortingGraph.tsx
+++ client/src/components/TheSortingGraph.tsx
@@ -7,11 +7,19 @@ interface Item {
 }
 
 export default function TheSortingGraph() {
-    const blockCount = 10;
-
+    const [blockCount, setBlockCount] = createSignal(10);
     const [blocks, setBlocks] = createSignal(genBlocks() as Item[]);
     const [sortedBlocks, setSortedBlocks] = createSignal([...blocks()] as Item[]);
     const [activeBlock, setActiveBlock] = createSignal(null as Item | null);
+    const [sortInterval, setSortInterval] = createSignal(null as number | null);
+
+    function clearSortInterval() {
+        if (sortInterval()) {
+            clearInterval(sortInterval() as number);
+            setSortInterval(null);
+        }
+    }
+
     let containerDiv: HTMLDivElement | undefined = undefined;
     function containerRect() {
         if (containerDiv) {
@@ -22,7 +30,7 @@ export default function TheSortingGraph() {
 
     function genBlocks() {
         const blocks = [];
-        for (let i = 0; i < blockCount; i++) {
+        for (let i = 0; i < blockCount(); i++) {
             blocks.push({
                 index: i,
                 value: i,
@@ -67,33 +75,133 @@ export default function TheSortingGraph() {
     }
 
     function bubbleSort() {
-        const interval = setInterval(() => {
+        clearSortInterval();
+        bubbleSortIteration();
+        const interval = window.setInterval(() => {
             if (!checkSorted()) {
                 bubbleSortIteration();
             } else {
                 clearInterval(interval);
             }
         }, 100);
+        setSortInterval(interval);
+    }
+
+    function bogoSort() {
+        clearSortInterval();
+        shuffleBlocks();
+        const interval = window.setInterval(() => {
+            if (!checkSorted()) {
+                shuffleBlocks();
+            } else {
+                clearInterval(interval);
+            }
+        }, 100);
+        setSortInterval(interval);
+    }
+
+    function mergeSort() {
+        clearSortInterval();
+        let stack: Item[][] = sortedBlocks().map(b => [b]);
+        stack = mergeSortIteration(stack);
+        const interval = window.setInterval(() => {
+            if (!checkSorted()) {
+                stack = mergeSortIteration(stack);
+                updateDOMBlocks();
+            } else {
+                clearInterval(interval);
+            }
+        }, 500);
+        setSortInterval(interval);
+    }
+
+    function merge(left: Item[], right: Item[]) {
+        const result: Item[] = [];
+        while (left.length > 0 && right.length > 0) {
+            if (left[0].value <= right[0].value) {
+                result.push(left.shift()!);
+            } else {
+                result.push(right.shift()!);
+            }
+        }
+        return result.concat(left, right);
+    }
+
+    // Iterative merge sort
+    function mergeSortIteration(stack: Item[][]) {
+        // Split stack until it's size is 1
+        console.log("mergeSortIteration");
+        if (stack.length === 1) {
+            return stack;
+        }
+
+        const newStack: Item[][] = [];
+        // Merge the stack so that it's halved
+        for (let i = 0; i < stack.length; i += 2) {
+            if (i + 1 < stack.length) {
+                newStack.push(merge(stack[i], stack[i + 1]));
+            } else {
+                newStack.push(stack[i]);
+            }
+        }
+
+        setSortedBlocks(matrixToArray(newStack));
+        return newStack;
+    }
+
+    function selectionSort() {
+        clearSortInterval();
+        let minIndex = 0;
+        const interval = window.setInterval(() => {
+            if (!checkSorted()) {
+                minIndex = selectionSortIteration(minIndex);
+                if (minIndex === blockCount() - 1) {
+                    clearInterval(interval);
+                }
+            } else {
+                clearInterval(interval);
+            }
+        }, 300);
+    }
+
+    function selectionSortIteration(minIndex: number) {
+        const newBlocks = [...sortedBlocks()];
+        let prevBlock = newBlocks[minIndex];
+        let minBlock = newBlocks[minIndex] || null;
+
+        // Find the minimum block
+        for (let i = minIndex; i < newBlocks.length; i++) {
+            if (minBlock === null || newBlocks[i].value < minBlock.value) {
+                minBlock = newBlocks[i];
+            }
+        }
+
+        // Remove the min block from the sorted blocks
+        newBlocks.splice(minBlock.index, 1);
+        // Move the minimum block to the minimum index
+        newBlocks.splice(minIndex, 0, minBlock);
+        minIndex++;
+        setSortedBlocks(newBlocks);
+        updateDOMBlocks();
+        return minIndex;
+    }
+
+    function matrixToArray(matrix: Item[][]) {
+        const result: Item[] = [];
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                result.push(matrix[i][j]);
+            }
+        }
+        return result;
     }
 
     function moveBlockToPosition(block: Item, final: Item) {
-        const newBlocks = [...blocks()];
-        if (block.index === final.index) {
-            return;
-        } else if (block.index < final.index) {
-            // Move all blocks between the two positions to the left
-            for (let i = block.index + 1; i <= final.index; i++) {
-                newBlocks.find(b => b.index === i)!.index--;
-            }
-            block.index = final.index + 1;
-        } else {
-            // Move all blocks between the two positions to the right
-            for (let i = block.index - 1; i >= final.index; i--) {
-                newBlocks.find(b => b.index === i)!.index++;
-            }
-            block.index = final.index - 1;
-        }
-        setBlocks(newBlocks);
+        const newBlocks = [...sortedBlocks()];
+        newBlocks.splice(block.index, 1);
+        newBlocks.splice(final.index, 0, block);
+        setSortedBlocks(newBlocks);
+        updateDOMBlocks();
     }
 
     function dragStart(e: MouseEvent, item: Item) {
@@ -113,8 +221,8 @@ export default function TheSortingGraph() {
         }
 
         // Split div into the amount of blocks
-        const brackets = containerRect()!.width / (blockCount);
-        const finalIndex = Math.min(x / brackets, blockCount - 1);
+        const brackets = containerRect()!.width / (blockCount());
+        const finalIndex = Math.min(x / brackets, blockCount() - 1);
         const finalIndexFloor = Math.floor(finalIndex);
         if (finalIndexFloor !== curBlock.index) {
             moveBlockToPosition(curBlock, blocks().find(b => b.index === finalIndexFloor)!);
@@ -141,6 +249,20 @@ export default function TheSortingGraph() {
         }
     }
 
+    function addBlock() {
+        setBlockCount(blockCount() + 1);
+        setBlocks(genBlocks())
+        setSortedBlocks(blocks());
+    }
+
+    function removeBlock() {
+        if (blockCount() > 2) {
+            setBlockCount(blockCount() - 1);
+            setBlocks(genBlocks())
+            setSortedBlocks(blocks());
+        }
+    }
+
     onMount(() => {
         window.addEventListener('mousemove', dragDuring);
         window.addEventListener('mouseup', dragEnd);
@@ -154,14 +276,13 @@ export default function TheSortingGraph() {
     return (
         <>
             <div class={styles.wrapper}>
-                <button onClick={shuffleBlocks}>Shuffle Blocks</button>
-                <button onClick={bubbleSort}>Bubble Sort</button>
+                <button onClick={() => { shuffleBlocks(); clearSortInterval(); }}>Shuffle Blocks</button>
                 <ul class={styles.graphList} ref={containerDiv}>
                     <For each={blocks()} >
                         {(item, index) => (
                             <li
                                 classList={{ [styles.graphItem]: true, [styles.graphItemActive]: activeBlock() === item }}
-                                style={{ left: (blocks()[index()].index) * (750 / blockCount) + "px", height: (item.value * 40 + 100) + "px" }}
+                                style={{ left: (blocks()[index()].index) * (750 / blockCount()) + "px", height: (item.value / blockCount() * 400 + 50) + "px", width: (500 / blockCount()) + "px" }}
                                 onMouseDown={(e) => dragStart(e, item)}
                                 onMouseEnter={(e) => hoverEnter(e, item)}
                                 onMouseLeave={(e) => hoverLeave(e)}
@@ -169,6 +290,13 @@ export default function TheSortingGraph() {
                         )}
                     </For>
                 </ul>
+                <button onClick={clearSortInterval}>Pause Sort</button>
+                <button onClick={bubbleSort}>Bubble Sort</button>
+                <button onClick={bogoSort}>Bogo Sort</button>
+                <button onClick={mergeSort}>Merge Sort</button>
+                <button onClick={selectionSort}>Selection Sort</button>
+                <button onClick={addBlock}>Add Block</button>
+                <button onClick={removeBlock}>Remove Block</button>
             </div>
         </>
     );
